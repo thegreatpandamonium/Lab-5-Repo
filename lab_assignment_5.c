1278f9cb4685862f5df605af0c8ea1de42ac81ed
Milan Castro <milan.castro1212@gmail.com>
2024-02-15 15:42:38 -0500

    Committing local changes before merging

--- lab_assignment_5.c
+++ lab_assignment_5.c
@@ -9,15 +9,26 @@ typedef struct node {
 // Returns number of nodes in the linkedList.
 int length(node* head)
 {
-   struct node *tmp = head;
-    int len = 0;
-   while (tmp != NULL)
-   {
-      tmp = tmp->next;
-      len++;
-   }
+   node* pTmp = head;
+    int lengthCount = 0;
 
-   return (len);
+    if (pTmp == NULL){
+        return lengthCount;
+    }
+
+    if (pTmp->next == NULL){
+        lengthCount=1;
+        return lengthCount;
+    }
+
+    while ((pTmp!= NULL) && (pTmp->next !=NULL)){
+        pTmp =  pTmp -> next ;
+        lengthCount ++;
+
+    }
+
+    return lengthCount;
+
 }
 
 // parses the string in the linkedList
@@ -25,6 +36,37 @@ int length(node* head)
 //  then toCString function wil return "abc"
 char* toCString(node* head)
 {
+    int lengthCount = length(head);
+    node* pTmp = head;
+    int i = 0; //Index
+
+    // DMA space, don't forget to add 1 for the null terminator
+    char* fullString = (char*)malloc((lengthCount+1)*sizeof(char));
+    
+    //Check to see if memory was allocated 
+    if (fullString == NULL){
+        return NULL;
+    }
+
+    //Check to see if pTmp is Null if yes return Null
+    if (pTmp == NULL){
+        return NULL;
+    }
+
+    while (pTmp != NULL){
+
+        fullString[i] =  pTmp->letter;
+        pTmp =  pTmp -> next ;
+        i++;
+
+    }
+
+    //Add null character
+    //fullString[i] = '\0';
+    fullString[lengthCount + 1] = '\0';
+
+
+    return fullString;
 }
 
 // inserts character to the linkedlist
@@ -33,11 +75,44 @@ char* toCString(node* head)
 // head -> |a|->|b|->|c|->|x|
 void insertChar(node** pHead, char c)
 {
+    //Create new node and dynamically allocate memory
+    node* newNode = (node*)malloc(sizeof(node));
+    node* pTmp = *pHead;
+
+    //Check if memory was allocated property
+    if (newNode == NULL){
+        return;
+    }
+
+    // Initialize the new node
+    newNode->letter = c;
+    newNode->next = NULL;
+
+    //Check Edge Conditions
+    if (pTmp == NULL){
+        //Update head so it eqals new node
+       *pHead = newNode;    
+        return;
+    }
+
+    //Tranverse LL
+    while (pTmp->next !=NULL){
+        pTmp =  pTmp -> next;  
+    }
+
+    pTmp->next = newNode;
 }
 
 // deletes all nodes in the linkedList.
 void deleteList(node** pHead)
 {
+   while(*pHead != NULL){
+
+        node* temp = *pHead;
+        *pHead = (*pHead)->next;
+        free(temp);
+    }
+    *pHead = NULL;
 }
 
 int main(void)
